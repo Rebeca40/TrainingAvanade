23d6197b132cfff5bc1839e974d3f3a778f84c1d
Rebeca Calles Conde <callesconderebeca@gmail.com>
2021-12-15 07:31:23 +0100

    Introducción de la clase book y de la interface IBookService

---
+++ Training.Core/Models/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Training.Core.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string ISBN { get; set; }
+        public string Name { get; set; }
+        public string Author { get; set; }
+    }
+}
